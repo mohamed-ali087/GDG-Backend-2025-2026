a2b20dfb63d6c435c3d94b75fc330a11132e90ff
Mohamed Ali <mohamedali100087@gmail.com>
2026-03-23 20:41:12 +0200

    PHP Assignments

--- PHP/Assignments/as12/index copy 2.php
+++ PHP/Assignments/as12/index copy 2.php
@@ -4,7 +4,7 @@ date_default_timezone_set("Africa/Cairo");
 
 $date = "1990-10-01";
 
-$seconds = strtotime($date); // عدد الثواني منذ Epoch
+$seconds = strtotime($date);
 
 $days = $seconds / 60 / 60 / 24;
 $years = $days / 365;
